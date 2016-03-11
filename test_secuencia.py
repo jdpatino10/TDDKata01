4a4d7d8c87861fcbeb03b3dcdabc5ef1a44f1059
jdpatino10 <jd.patino10@uniandes.edu.co>
2016-03-11 00:50:54 -0500

    Iteración 1-Prueba con un número (Rojo)

--- test_secuencia.py
+++ test_secuencia.py
@@ -6,3 +6,6 @@ from Secuencia import Secuencia
 class TestSecuencia(TestCase):
     def test_cadenaVacia(self):
         self.assertEqual(Secuencia().verificarSecuencia("")[0], 0, "Cadena vacia")
+
+    def test_conUnNumero(self):
+        self.assertEqual(Secuencia().verificarSecuencia("2")[0], 1, "Con un numero")
