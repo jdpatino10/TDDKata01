4784da006d73cfdd3ab8441d06c59e4e116a7e05
jdpatino10 <jd.patino10@uniandes.edu.co>
2016-03-11 01:28:13 -0500

    Iteración 2-Prueba con N números (Verde)

--- Secuencia.py
+++ Secuencia.py
@@ -4,9 +4,7 @@ class Secuencia:
             return [0,0]
         elif "," in cadena:
             arreglo=cadena.split(",")
-            minimo=arreglo[1]
-            if int(arreglo[0]) < int(arreglo[1]):
-                minimo=arreglo[0]
-            return [len(arreglo), int(minimo)]
+            arreglo=map(int, arreglo)
+            return [len(arreglo), min(arreglo)]
         else:
             return [1,int(cadena)]
