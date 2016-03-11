84511ca0e9d394538ce32e96c3495677ae76a5f5
jdpatino10 <jd.patino10@uniandes.edu.co>
2016-03-11 01:23:55 -0500

    Iteración 2-Prueba con dos números (Verde)

--- Secuencia.py
+++ Secuencia.py
@@ -4,6 +4,9 @@ class Secuencia:
             return [0,0]
         elif "," in cadena:
             arreglo=cadena.split(",")
-            return [len(arreglo)]
+            minimo=arreglo[1]
+            if int(arreglo[0]) < int(arreglo[1]):
+                minimo=arreglo[0]
+            return [len(arreglo), int(minimo)]
         else:
             return [1,int(cadena)]
