17a9b17d0a6b944d46a2d3348e927459b55b383b
cado10 <ca.delgadoo@uniandes.edu.co>
2016-03-15 13:51:34 -0500

    Paso 3: Se corrige para dos números (Verde)

--- Secuencia.py
+++ Secuencia.py
@@ -5,8 +5,13 @@ class Secuencia:
         elif "," in cadena:
             arreglo=cadena.split(",")
             arreglo=map(int, arreglo)
+            suma=0.0
+            prome=0.0
+            if len(arreglo)==2:
+                suma= suma + arreglo[0]+ arreglo[1]
+            prome=suma/2
 
-            return [len(arreglo), min(arreglo), max(arreglo)]
+            return [len(arreglo), min(arreglo), max(arreglo), prome]
 
         else:
             return [1,int(cadena), int(cadena), int(cadena)]
