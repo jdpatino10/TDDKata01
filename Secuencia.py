2f1463b9b86da7fa489e8af7174fb2d5f5afd630
cado10 <ca.delgadoo@uniandes.edu.co>
2016-03-15 12:50:52 -0500

    Paso 3: Se corrige  para dos numeros (Verde)

--- Secuencia.py
+++ Secuencia.py
@@ -5,6 +5,13 @@ class Secuencia:
         elif "," in cadena:
             arreglo=cadena.split(",")
             arreglo=map(int, arreglo)
-            return [len(arreglo), min(arreglo)]
+
+            maximo=arreglo[1]
+            if int(arreglo[0]) > int(arreglo[1]):
+                maximo=arreglo[0]
+
+
+            return [len(arreglo), min(arreglo), int(maximo)]
+
         else:
             return [1,int(cadena), int(cadena)]
